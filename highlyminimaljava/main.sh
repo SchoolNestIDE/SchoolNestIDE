28f600b22cb28e07b19ffa488a947850cd25feb1
OBantanur <omkar.bantanur@gmail.com>
2025-06-15 17:37:48 -0400

    removed unnecesary to hopefully mitigate merge conflicts

--- highlyminimaljava/main.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-sed  "s/((SIZE))/$(stat -c"%s" ../public/disk)/" init_template > ./init
-docker build --platform linux/i386 -t nestdocker_larger .
-docker run -d -it --name nestdocker_larger nestdocker_larger
-docker export nestdocker_larger > output.tar
-MOUNT_DIR=$(mktemp -d)
-
-tar2sqfs --compressor zstd  ../public/disk.larger < output.tar
--- minidisk/run_in_chroot.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-set +x
-/tmp/new-chroot/lib/ld-linux.so.2 --inhibit-cache --library-path /tmp/new-chroot/lib/i386-linux-gnu:/tmp/new-chroot/usr/lib/i386-linux-gnu /tmp/new-chroot/tmp/jdk/bin/$@
